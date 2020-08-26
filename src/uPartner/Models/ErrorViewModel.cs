4a36a695097fd229025dea589813b6e5c9105067
Lincoln Vieira <lincoln_vieira@outlook.com.br>
2020-08-25 21:33:39 -0300

    Commit inicial

---
+++ src/uPartner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace uPartner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
